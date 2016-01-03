573c5613d2e38c6d96a8ad92298d20eb19b6b0e1
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2016-01-03 16:46:00 +0800

    plot 2 hist in one image

--- matplotlib/hist_normal_multiple_overlay.py
+++ matplotlib/hist_normal_multiple_overlay.py
@@ -4,7 +4,8 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 
-FILENAME = "./data/HB101-N2-before-worm1_to_15.dat"
+FILENAME_1 = "./data/HB101-N2-before-worm1_to_15.dat"
+FILENAME_2 = "./data/HB101-N2-after-worm1_to_10.dat"
 
 
 # read data from a text file. One number per line
@@ -27,21 +28,32 @@ def get_data_list(filename):
     return data_list
 
 
-# best fit of data
-data_list = get_data_list(FILENAME)
-(mu, sigma) = norm.fit(data_list)
+def plot_hist_and_fit_gauss(data_list, binno=60, normed=1, facecolor="green", alpha=0.75, linecolor='r--'):
+    """
+    return (mu, sigma)
+    """
+    # fit the data by guass distribution
+    # norm.fit returns (mu, sigma)
+    (mu, sigma) = norm.fit(data_list)
+    # plot the histogram of the data
+    (n, bins, patches) = plt.hist(data_list, binno, normed=normed, facecolor=facecolor, alpha=alpha)
+    # add a 'best fit' line
+    y = mlab.normpdf(bins, mu, sigma)
+    l = plt.plot(bins, y, linecolor, linewidth=2)
+    return (mu, sigma)
 
-# the histogram of the data
-n, bins, patches = plt.hist(data_list, 60, normed=1, facecolor='green', alpha=0.75)
+data_list_1 = get_data_list(FILENAME_1)
+mu_sigma_1 = plot_hist_and_fit_gauss(data_list_1, facecolor='cyan', linecolor='g--')
 
-# add a 'best fit' line
-y = mlab.normpdf( bins, mu, sigma)
-l = plt.plot(bins, y, 'r--', linewidth=2)
+data_list_2 = get_data_list(FILENAME_2)
+mu_sigma_2 = plot_hist_and_fit_gauss(data_list_2, facecolor='magenta')
+
+mu_sigma = mu_sigma_1 + mu_sigma_2
 
 #plot
 plt.xlabel('Speed')
 plt.ylabel('Probability')
-plt.title(r'$\mathrm{Histogram\ of\ Bug\ Speed:}\ \mu=%.3f,\ \sigma=%.3f$' %(mu, sigma))
+plt.title(r'$\mathrm{Histogram\ of\ Bug\ Speed:}\ \mu1=%.3f,\ \sigma1=%.3f,\ \mu2=%.3f,\ \sigma2=%.3f$' % mu_sigma)
 plt.grid(True)
 
 plt.show()
