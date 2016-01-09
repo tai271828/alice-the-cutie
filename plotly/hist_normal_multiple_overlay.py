74d0feccf3f8114789099457468107b68c597e56
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2016-01-10 00:45:31 +0800

    add threshold and pair comparison

--- plotly/hist_normal_multiple_overlay.py
+++ plotly/hist_normal_multiple_overlay.py
@@ -1,34 +1,56 @@
 #!/usr/bin/python
 import plotly.plotly as py
 from plotly.tools import FigureFactory as FF
-
 import numpy as np
+import posixpath
+
+
+THRESHOLD = 45
+
+
 # read data from a text file. One number per line
-def get_data_list(filename):
-    arch = filename
-    datos = []
-    for item in open(arch,'r'):
-        item = item.strip()
-        if item != '':
-            try:
-                datos.append(float(item))
-            except ValueError:
-                pass
-    return datos
-data_01 = get_data_list("alice-is-cute-HB101afterN2.txt")
-data_02 = get_data_list("alice-is-cute-HB101afterN2-02.txt")
+def rm_bad_data(data, threshold):
+    rtn_data = []
+    for item in data:
+        if item < threshold:
+            rtn_data.append(item)
+    return rtn_data
 
-hist_data = [data_01, data_02]
 
-group_labels = ['Group 1', 'Group 2']
+def get_data(filename):
+    data = []
+    f = open(filename, "r")
+    lines = f.readlines()
+    for line in lines:
+        line.strip()
+        try:
+            if line != '':
+                data.append(float(line))
+        except ValueError:
+            pass
+    if THRESHOLD:
+        data = rm_bad_data(data, THRESHOLD)
+    return data
 
-colors = ['rgb(0, 0, 100)', 'rgb(300, 200, 200)']
 
-# Create distplot with curve_type set to 'normal'
-fig = FF.create_distplot(hist_data, group_labels, bin_size=.5, curve_type='normal', colors=['rgb(0, 0, 100)', 'rgb(0, 200, 200)'])
+for fileno in range(1, 17, 2):
+    filename1 = "../data/160109/arr" + str(fileno) + "_1.txt"
+    filename2 = "../data/160109/arr" + str(fileno + 1) + "_1.txt"
+    data_01 = get_data(filename1)
+    data_02 = get_data(filename2)
 
-# Add title
-fig['layout'].update(title='Alice is so cute so I plot this for her!!!')
+    hist_data = [data_01, data_02]
 
-# Plot!
-py.iplot(fig, filename='two-cute-plots-and-sleepy', validate=False)
+    group_labels = [posixpath.basename(filename1), posixpath.basename(filename2)]
+
+    colors = ['rgb(0, 0, 100)', 'rgb(300, 200, 200)']
+
+    # Create distplot with curve_type set to 'normal'
+    fig = FF.create_distplot(hist_data, group_labels, bin_size=.5, curve_type='normal', colors=['rgb(0, 0, 100)', 'rgb(0, 200, 200)'])
+
+    # Add title
+    #fig['layout'].update(title='Alice is so cute so I plot this for her!!!')
+
+    # Plot!
+    py.iplot(fig, filename="160109-histogram-1/" + str(fileno), validate=False)
+
